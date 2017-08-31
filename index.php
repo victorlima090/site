50e224414e48a5594758f06280ffe8a158e7174a
victorlima090 <victorlima090@JarvasL>
2017-08-31 13:54:43 -0300

    teste pra ver se index funciona

--- index.php
+++ index.php
@@ -1,12 +1,17 @@
-<!DOCTYPE html>
+    
+    <!DOCTYPE html>
         <html>
             <head>
-                <title>TODO supply a title</title>
+                <title>Nafisio</title>
                 <meta charset="UTF-8">
                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
             </head>
             <body>
-                <div>alguma coisa</div>
+                <h2>Bem vindo a Nafisio HomePageee</h2>
+                <br/>
                 <div><a href="login.php">Login</a></div>
+                <br/>
+                <div><a href="pagina1.php">Pagina 1</a></div>
             </body>
-        </html>     
+        </html>
+     
